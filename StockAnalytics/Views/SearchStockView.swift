b39be5838b1cfc607a86922cfc9863b3d631a444
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-03-22 17:18:28 +0530

    Added SearchBar view

---
+++ StockAnalytics/Views/SearchStockView.swift
@@ -0,0 +1,37 @@
+//
+//  SearchStockView.swift
+//  StockAnalytics
+//
+//  Created by Akshit Zaveri on 22/03/20.
+//  Copyright © 2020 Akshit Zaveri. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchStockView: View {
+
+  private enum Constants {
+    static let searchBarPlaceholder = "Search Stocks"
+  }
+
+  /// Search text entered by the user
+  @State private var searchText = ""
+
+  var body: some View {
+    NavigationView {
+      VStack {
+        SearchBar(
+          text: $searchText,
+          placeholder: Constants.searchBarPlaceholder
+        )
+        Spacer()
+      }.navigationBarTitle(Text("Search"))
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    SearchStockView()
+  }
+}
