6a78f5cc073257c64d771c3d67076a6f91a1ff62
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-03-24 16:22:34 +0530

    Added tests for XCConfig file. Added server url key to configuration

--- StockAnalytics/XCode Configurations/Configuration.swift
+++ StockAnalytics/XCode Configurations/Configuration.swift
@@ -3,31 +3,34 @@ import Foundation
 /// A class which extracts the value for the provided key for the current build configuration
 enum Configuration {
 
-    /// Errors
-    enum Error: Swift.Error {
-        case missingKey, invalidValue
+  /// Errors
+  enum Error: Swift.Error {
+    case missingKey, invalidValue
+  }
+
+  /// List of keys available in the build configuration files
+  enum Key {
+    static let serverURLString = "SERVER_URL_STRING"
+  }
+
+  /// Extracts the value for the specified key and throws the error if there's no value or it's invalid value
+  /// - Parameter key: Key for which the value needs to be extracted
+  static func value<T>(
+    for key: String,
+    in bundle: Bundle = .main
+  ) throws -> T where T: LosslessStringConvertible {
+    guard let object = bundle.object(forInfoDictionaryKey: key) else {
+      throw Error.missingKey
     }
 
-    /// List of keys available in the build configuration files
-    enum Key: String {
-        case temp // Temporary key
+    switch object {
+    case (let value as T):
+      return value
+    case (let string as String):
+      guard let value = T(string) else { fallthrough }
+      return value
+    default:
+      throw Error.invalidValue
     }
-
-    /// Extracts the value for the specified key and throws the error if there's no value or it's invalid value
-    /// - Parameter key: Key for which the value needs to be extracted
-    static func value<T>(for key: Key) throws -> T where T: LosslessStringConvertible {
-        guard let object = Bundle.main.object(forInfoDictionaryKey: key.rawValue) else {
-            throw Error.missingKey
-        }
-
-        switch object {
-        case (let value as T):
-            return value
-        case (let string as String):
-            guard let value = T(string) else { fallthrough }
-            return value
-        default:
-            throw Error.invalidValue
-        }
-    }
+  }
 }
---
+++ StockAnalyticsTests/XCode Configurations/ConfigurationTests.swift
@@ -0,0 +1,119 @@
+//
+//  ConfigurationTests.swift
+//  StockAnalyticsTests
+//
+//  Created by Akshit Zaveri on 24/03/20.
+//  Copyright © 2020 Akshit Zaveri. All rights reserved.
+//
+
+import XCTest
+@testable import StockAnalytics
+
+class ConfigurationTests: XCTestCase {
+
+  private enum Constants {
+    static let integerValueMock = 10
+    static let floatValueMock: Float = 10.65
+  }
+
+  private enum Key {
+    static let integerKey = "integerKey"
+    static let floatKey = "floatKey"
+    static let stringKey = "stringKey"
+    static let missingKey = "missingKey"
+    static let urlKey = "urlKey"
+    static let booleanTrueKey = "booleanTrueKey"
+    static let booleanFalseKey = "booleanFalseKey"
+  }
+
+  private func getCurrentBundle() -> Bundle {
+    Bundle(for: self.classForCoder)
+  }
+
+  func test_WhenStringIsRequested_ThenStringIsFetched() {
+    // given
+
+    // when
+    let result: String? = try? Configuration.value(
+      for: Key.stringKey,
+      in: getCurrentBundle()
+    )
+
+    // then
+    XCTAssertEqual(result, "https://stock-analytics-server.herokuapp.com/api/stocks")
+  }
+
+  func test_WhenInvalidKeyIsRequested_ThenErrorIsReturned() {
+    // given
+
+    // when
+    let result: String?
+    do {
+      result = try Configuration.value(
+        for: Key.missingKey,
+        in: getCurrentBundle()
+      )
+
+      // then
+      XCTAssertEqual(result, nil)
+    } catch Configuration.Error.missingKey {
+      // Do nothing as this is expected to execute
+    } catch {
+      // then
+      XCTAssertNoThrow(Configuration.Error.missingKey)
+    }
+  }
+
+  func test_WhenIntegerRequested_ThenIntegerIsReturned() {
+    // given
+    let bundle = Bundle(for: self.classForCoder)
+
+    // when
+    let result: Int? = try? Configuration.value(
+      for: Key.integerKey,
+      in: bundle
+    )
+
+    // then
+    XCTAssertEqual(result, Constants.integerValueMock)
+  }
+
+  func test_WhenFloatRequested_ThenFloatIsReturned() {
+    // given
+
+    // when
+    let result: Float? = try? Configuration.value(
+      for: Key.floatKey,
+      in: getCurrentBundle()
+    )
+
+    // then
+    XCTAssertEqual(result, Constants.floatValueMock)
+  }
+
+  func test_WhenTrueBooleanRequested_ThenTrueBooleanIsReturned() {
+    // given
+
+    // when
+    let result: Bool! = try? Configuration.value(
+      for: Key.booleanTrueKey,
+      in: getCurrentBundle()
+    )
+
+    // then
+    XCTAssertTrue(result)
+  }
+
+  func test_WhenFalseBooleanRequested_ThenFalseBooleanIsReturned() {
+    // given
+
+    // when
+    let result: Bool! = try? Configuration.value(
+      for: Key.booleanFalseKey,
+      in: getCurrentBundle()
+    )
+
+    // then
+    XCTAssertFalse(result)
+  }
+}
